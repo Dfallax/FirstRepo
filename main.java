9c680317bd74f82555aef2d57f0821dd61a5e007
Dfallax <dfalla0001@gmail.com>
2023-10-09 09:25:27 +0200

    Main prints fFarewell

--- main.java
+++ main.java
@@ -1,4 +1,6 @@
 public static void MAin(String[] args) {
 	String salutation = "Hola Mundo";
 	System.out.println(salutation);
+	String Firewell = "Adiós con los ojos";
+	System.out.pintln(Firewell);
 }
